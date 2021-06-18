64f86053ef11f34d6209d070c6286a7501b24b3a
jfcann <jamescann@live.com>
2021-06-18 14:57:26 +0100

    introducing itemid to token mapping in preprocessing

--- apps/mimic-iii/preprocessing.py
+++ apps/mimic-iii/preprocessing.py
@@ -1,15 +1,17 @@
+
+import os
 import numpy as np
 import pandas as pd
 import pickle as pickle
 import torch
-from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 chartevents_path = "C:/Users/james/Data/MIMIC/mimic-iii-clinical-database-1.4/CHARTEVENTS.csv"
 admissions_path = "C:/Users/james/Data/MIMIC/mimic-iii-clinical-database-1.4/ADMISSIONS.csv"
 d_items_path = "C:/Users/james/Data/MIMIC/mimic-iii-clinical-database-1.4/d_items.csv"
-save_path = 'C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers/train_charts.pkl'
+save_root = 'C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers'
 
 # read in admissions
 admissions = pd.read_csv(admissions_path)
@@ -19,12 +21,10 @@ admissions['ADMITTIME'] = pd.to_datetime(admissions['ADMITTIME'])
 admittimes = admissions[admissions.HAS_CHARTEVENTS_DATA == 1][['SUBJECT_ID', 'HADM_ID', 'ADMITTIME']]
 
 # pick the earliest admission times for each subject.
-admitfirsttimes = \
-    pd.merge(admittimes[['SUBJECT_ID', 'ADMITTIME']] \
-             .groupby(by='SUBJECT_ID').min(),
-             admittimes,
-             how='left',
-             on=['SUBJECT_ID', 'ADMITTIME'])
+admitfirsttimes = pd.merge(admittimes[['SUBJECT_ID', 'ADMITTIME']].groupby(by='SUBJECT_ID').min(),
+                           admittimes,
+                           how='left',
+                           on=['SUBJECT_ID', 'ADMITTIME'])
 
 assert len(admitfirsttimes) == admittimes['SUBJECT_ID'].nunique()
 assert admitfirsttimes['HADM_ID'].nunique() == admitfirsttimes['SUBJECT_ID'].nunique()
@@ -35,13 +35,13 @@ all_indices = admitfirsttimes.index.to_numpy()
 
 # split first-hadm_ids into train, val, test and check.
 
-train_indices, surplus = np.split(all_indices, [int(0.8 * all_indices.size)])
-val_indices, test_indices = np.split(surplus, [int(0.5 * surplus.size)])
+train_indices, surplus = train_test_split(all_indices, train_size=0.8)
+val_indices, test_indices = train_test_split(surplus, test_size=0.5)
 del surplus
 assert set(all_indices) == set(train_indices) | set(val_indices) | set(test_indices)
 
-
 # helpers
+
 
 def ts_to_posix(time):
     return pd.Timestamp(time, unit='s').timestamp()
@@ -52,43 +52,73 @@ def get_admittime(hadm_id):
     return ts_to_posix(time)
 
 
-# grouper
+# token mappings
 
-gpdf = (pd.read_csv(chartevents_path, skiprows=0, nrows=100000,
-                    header=0,
-                    usecols=['HADM_ID', 'CHARTTIME', 'ITEMID'],
-                    dtype={'HADM_ID': np.int, 'ITEMID': str},
-                    parse_dates=['CHARTTIME'])
-        .query('HADM_ID.isin(@train_indices)')
-        .groupby(by='HADM_ID')
+d_items = pd.read_csv(d_items_path)
+num_tokens = len(d_items)
+
+itemid2token = dict(zip(d_items['ITEMID'], range(len(d_items))))
+token2itemid = {v: k for k, v in itemid2token.items()}
+token2label = dict(zip(range(len(d_items)), d_items['LABEL']))
+
+
+with open(os.path.join(save_root, 'itemid2token.pkl'), 'wb') as f:
+    pickle.dump(itemid2token, f)
+
+
+def map2token(itemid):
+    return itemid2token[np.int(itemid)]
+
+
+def map2itemid(token):
+    return str(token2itemid[token])
+
+
+def map2itemidstr(tokens):
+    return ' '.join(list(map(map2itemid, tokens)))
+
+
+# loop through sets and generate output files
+
+for subset in ['train', 'val', 'test']:
+
+    # grouper
+
+    gpdf = (pd.read_csv(chartevents_path, skiprows=0, nrows=10000000,
+                        header=0,
+                        usecols=['HADM_ID', 'CHARTTIME', 'ITEMID'],
+                        dtype={'HADM_ID': np.int},
+                        converters={'ITEMID': map2token},
+                        parse_dates=['CHARTTIME'])
+            .query(f'HADM_ID.isin(@{subset}_indices)')
+            .groupby(by='HADM_ID')
+            )
+
+    # initialise
+
+    items = dict()
+    times = dict()
+    times_rel = dict()
+
+    # populate with entries
+
+    for i in gpdf.groups:
+        time_origin = get_admittime(i)
+        temp = gpdf.get_group(i).sort_values(by="CHARTTIME")
+        items[i] = np.array(temp['ITEMID'], dtype=int)
+        times[i] = np.fromiter(
+            map(ts_to_posix, temp['CHARTTIME']),
+            dtype=np.int64
         )
+        times_rel[i] = times[i] - time_origin
 
-# initialise
+    # write out dicts to pickle
 
-train_items = dict()
-train_times = dict()
-train_times_rel = dict()
+    save_path = os.path.join(save_root, f'{subset}_charts.pkl')
 
-# populate with entries
+    with open(save_path, 'wb') as f:
+        pickle.dump({f'{subset}_items': items,
+                     f'{subset}_times': times,
+                     f'{subset}_times_rel': times_rel}, f)
 
-for i in gpdf.groups:
-    time_origin = get_admittime(i)
-    temp = gpdf.get_group(i).sort_values(by="CHARTTIME")
-    train_items[i] = np.array(temp['ITEMID'], dtype=int)
-    train_times[i] = np.fromiter(
-        map(ts_to_posix, temp['CHARTTIME']),
-        dtype=np.int64
-    )
-    train_times_rel[i] = train_times[i] - time_origin
-
-# note - need to be sure of whole sequence before normalising.
-
-# write out dicts to pickle
-with open(save_path, 'wb') as f:
-    pickle.dump(
-        {'train_items': train_items,
-         'train_times': train_times,
-         'train_times_rel': train_times_rel
-         },
-        f
-    )
+    del items, times, times_rel, gpdf
