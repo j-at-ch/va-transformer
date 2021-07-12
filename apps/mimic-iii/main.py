5ab7281486361fa298d373966df076a4819aa55d
jfcann <jamescann@live.com>
2021-07-12 12:42:09 +0100

    add argparsing for main

--- apps/mimic-iii/main.py
+++ apps/mimic-iii/main.py
@@ -1,11 +1,17 @@
 import torch
+import methods
+import data_utils
+from arguments import Arguments
+from pprint import pprint
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def main():
-    print("main.py")
+def main(args):
+    print('*'*17, 'chart-transformer called with the following settings:', sep='\n')
+    pprint(vars(args), indent=2)
 
 
 if __name__ == "__main__":
-    main()
+    arguments = Arguments().parse()
+    main(arguments)
