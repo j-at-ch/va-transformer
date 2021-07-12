ee3f4ff2a9c5523a9344dbb5726f81fce469cea1
jfcann <jamescann@live.com>
2021-07-12 18:56:09 +0100

    improved data utils

--- apps/mimic-iii/data_utils.py
+++ apps/mimic-iii/data_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 import torch
 
 from torch.utils.data import DataLoader, Dataset
@@ -29,3 +30,54 @@ class ClsSamplerDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+def cycle(loader):
+    while True:
+        for data in loader:
+            yield data
+
+
+class Mappings:
+    def __init__(self, mappings):
+        self.itemid2token = mappings['itemid2token']
+        self.token2itemid = mappings['token2itemid']
+        self.num_tokens = len(self.itemid2token)
+
+    def decode_token(self, token):
+        return str(self.token2itemid[token])
+
+    def decode_tokens(self, tokens):
+        return ' '.join(list(map(self.decode_token, tokens)))
+
+
+class Labellers(Mappings):
+    def __init__(self, mappings, d_items_df):
+        super().__init__(mappings)
+        self.mappings = mappings
+        self.d_items_df = d_items_df
+
+    def token2label(self, token):
+        if token == 0:
+            return '[PAD]'
+        else:
+            itemid = self.token2itemid[token]
+            x = self.d_items_df.loc[itemid, 'LABEL']
+        return x
+
+    def tokens2labels(self, tokens):
+        return '\n\t -> '.join(list(map(self.token2label, tokens)))
+
+
+def fetch_mappings(mapping_path):
+    with open(mapping_path, 'rb') as f:
+        mappings = pickle.load(f)
+    return mappings
+
+
+def fetch_data_as_torch(path, var_key):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    di = data[var_key]
+    return {k: torch.from_numpy(v) for k, v in di.items()}
+
