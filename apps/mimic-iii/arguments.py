9213504e3c43edd444d5af9a907dfb4b48e3fde6
jfcann <jamescann@live.com>
2021-07-12 18:55:21 +0100

    args now specified via arguments.py

--- apps/mimic-iii/arguments.py
+++ apps/mimic-iii/arguments.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from pprint import pprint
 
@@ -8,44 +9,39 @@ class Arguments:
         self.arguments = None
 
     def initialise(self):
+
         # data roots
 
-        self.parser.add_argument('--mimic_root', type=str,
-                                 default='C:/Users/james/Data/MIMIC/mimic-iii-clinical-database-1.4', help='')
-        self.parser.add_argument('--data_root', type=str,
-                                 default='C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers', help='')
-        self.parser.add_argument('--save_root', type=str,
-                                 default='C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers', help='')
+        self.parser.add_argument('--mimic_root', type=str)
+        self.parser.set_defaults(mimic_root='C:/Users/james/Data/MIMIC/mimic-iii-clinical-database-1.4')
+        self.parser.add_argument('--data_root', type=str)
+        self.parser.set_defaults(data_root='C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers')
+        self.parser.add_argument('--save_root', type=str)
+        self.parser.set_defaults(save_root='C:/Users/james/Data/MIMIC/mimic-iii-chart-transformers')
 
         # constants
 
-        self.parser.add_argument('--NUM_EPOCHS', type=int, default=10)
-        self.parser.add_argument('--NUM_BATCHES', type=int, default=100)
-        self.parser.add_argument('--BATCH_SIZE', type=int, default=4)
-        self.parser.add_argument('--GRADIENT_ACCUMULATE_EVERY', type=int, default=4)
-        self.parser.add_argument('--LEARNING_RATE', type=float, default=1e-4)
-        self.parser.add_argument('--VALIDATE_EVERY', type=int, default=10)
-        self.parser.add_argument('--CHECKPOINT_AFTER', type=int, default=10)
-        self.parser.add_argument('--GENERATE_EVERY', type=int, default=20)
-        self.parser.add_argument('--GENERATE_LENGTH', type=int, default=200)
-        self.parser.add_argument('--SEQ_LEN', type=int, default=200)
+        self.parser.add_argument('--num_epochs', type=int, default=10)
+        self.parser.add_argument('--num_batches_tr', type=int, default=1000)
+        self.parser.add_argument('--num_batches_val', type=int, default=1000)
+        self.parser.add_argument('--batch_size_tr', type=int, default=4)
+        self.parser.add_argument('--batch_size_val', type=int, default=4)
+        self.parser.add_argument('--grad_accumulate_every', type=int, default=4)
+        self.parser.add_argument('--learning_rate', type=float, default=1e-4)
+        self.parser.add_argument('--validate_every', type=int, default=10)
+        self.parser.add_argument('--checkpoint_after', type=int, default=100)
+        self.parser.add_argument('--generate_every', type=int, default=20)
+        self.parser.add_argument('--generate_length', type=int, default=200)
+        self.parser.add_argument('--seq_len', type=int, default=200)
 
         # model specification
 
-        self.parser.add_argument('--max_norm', dest='max_norm', action='store_true')
-        self.parser.add_argument('--linear_size', type=int, default=256, help='size of each model layer')
-        self.parser.add_argument('--num_stage', type=int, default=12, help='# layers in linear model')
-
-        # training specification
-
-        self.parser.add_argument('--lr', type=float)
-
-        # set defaults
-
-        self.parser.set_defaults(max_norm=True)
+        self.parser.add_argument('--attn_dim', type=int, default=100)
+        self.parser.add_argument('--attn_depth', type=int, default=3)
+        self.parser.add_argument('--attn_heads', type=int, default=4)
 
     def parse(self, verbose=False):
         self.initialise()
         self.arguments = self.parser.parse_args()
-        if verbose: pprint(vars(self.arguments), indent=2)
+        if verbose: pprint(vars(self.arguments), indent=4)
         return self.arguments
